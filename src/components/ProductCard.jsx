be7d41dc2d553295beb053941ac19cd0da82aa13
Brad <brthomiu@gmail.com>
2022-10-31 21:42:19 -0600

    Add functionality to add and remove items from cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,40 @@
 import { Card, Button, Form, Row, Col } from "react-bootstrap";
+import { CartContext } from "../cartContext";
+import { useContext } from "react";
 
 const ProductCard = (props) => {
   const product = props.product;
+  const cart = useContext(CartContext);
+  const productQuantity = cart.getProductQuantity(product.id);
+  console.log(cart.items);
 
   return (
     <Card>
       <Card.Body>
         <Card.Title>{product.title}</Card.Title>
         <Card.Text>${product.price}</Card.Text>
-        <Button variant="primary">Add to Cart</Button>
+        {productQuantity > 0 ? (
+          <>
+            <Form as={Row}>
+              <Form.Label column="true" sm="6">
+                In Cart: {productQuantity}
+              </Form.Label>
+              <Col sm="6" align="center">
+                <Button sm="6" className="m-2" 
+                onClick={() => cart.addOneToCart(product.id)}>+</Button>
+                <Button sm="6" className="m-2" 
+                onClick={() => cart.removeOneFromCart(product.id)}>-</Button>
+              </Col>
+            </Form>
+          </>
+        ) : (
+          <Button
+            variant="primary"
+            onClick={() => cart.addOneToCart(product.id)}
+          >
+            Add to Cart
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
